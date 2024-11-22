30ee0af44a8830816919e8656634c3879bfa051e
Robson Rodrigues Junior <robson.rodrigues477557@outlook.com>
2024-11-21 23:37:04 -0300

    Inserir frontend no Laravel

---
+++ resources/views/xdebug_info.php
@@ -0,0 +1,1 @@
+<?php echo xdebug_info();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 Route::get('/', function () {
-    return view('welcome');
+    View::addExtension('html', 'php');
+    return View::make('home');
+});
+
+Route::get('/phpinfo', function () {
+    View::addExtension('html', 'php');
+    return View::make('phpinfo');
+});
+
+Route::get('/xdebug_info', function () {
+    View::addExtension('html', 'php');
+    return View::make('xdebug_info');
 });
